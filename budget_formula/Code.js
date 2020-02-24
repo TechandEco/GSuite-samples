56cc563040c784565b8a27aefa086f914cf64967
AGV <agv@google.com>
2020-02-23 22:46:53 -0800

    fixed indentation

--- budget_formula/Code.js
+++ budget_formula/Code.js
@@ -8,29 +8,29 @@
  * @returns {number} - annual cost
  */
 function ANNUAL_COST(amount, frequency) {
-    return amount * timesPerYear(frequency);
-  }
+  return amount * timesPerYear(frequency);
+}
 
-  /**
-   * @param {string} frequency - frequency as a string.
-   *
-   * @return {number} - number of times per year.
-   */
-  function timesPerYear(frequency) {
-    switch (frequency) {
-      case '1 time':
-        return 1;
-      case 'Weekly':
-        return 52;
-      case '2 weeks':
-        return 52 / 2;
-      case 'Monthly':
-        return 12;
-      case '2 months':
-        return 6;
-      case 'Semester (6 months)':
-        return 2;
-      case 'Yearly':
-        return 1;
-    }
+/**
+ * @param {string} frequency - frequency as a string.
+ *
+ * @return {number} - number of times per year.
+ */
+function timesPerYear(frequency) {
+  switch (frequency) {
+    case '1 time':
+      return 1;
+    case 'Weekly':
+      return 52;
+    case '2 weeks':
+      return 52 / 2;
+    case 'Monthly':
+      return 12;
+    case '2 months':
+      return 6;
+    case 'Semester (6 months)':
+      return 2;
+    case 'Yearly':
+      return 1;
   }
+}
